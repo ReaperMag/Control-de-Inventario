46db0019c3b14b6cd4e8224f188531a65f40ded7
Jair Mera <j.meracount@gmail.com>
2024-12-07 21:06:45 -0500

    db.init

--- app.py
+++ app.py
@@ -2,31 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 from src import crear_app
-from src.imports import db
 
-def inicializar_materiales():
-    materiales = ["Tubos", "Cartillas", "Tubos tapa Verde"]
-    from src.Models.models import Materiales
-    
-    if not Materiales.query.first():
-        for m in materiales:
-            nuevo_material = Materiales(m,0)
-            db.session.add(nuevo_material)
-        db.session.commit()
-        print("Materiales iniciales creados")
-    else:
-        print("No se insertaron materiales")
 
 app = crear_app()
 # app.config.from_object(Config)
 # print(app.config)
 
-db.init_app(app)
 
-with app.app_context():
 
-    db.create_all()
-    inicializar_materiales()
+
 
 if __name__ == "__main__":
     
--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,20 @@
 from flask import Flask
 from config import Config
+from src.imports import db
+
+
+def inicializar_materiales():
+    materiales = ["Tubos", "Cartillas", "Tubos tapa Verde"]
+    from src.Models.models import Materiales
+    
+    if not Materiales.query.first():
+        for m in materiales:
+            nuevo_material = Materiales(m,0)
+            db.session.add(nuevo_material)
+        db.session.commit()
+        print("Materiales iniciales creados")
+    else:
+        print("No se insertaron materiales")
 
 def crear_app():
     from src.routes.route import bp
@@ -8,5 +23,12 @@ def crear_app():
     
     app.register_blueprint(bp)
     app.config.from_object(Config)
+    
+    db.init_app(app)
+    
+    with app.app_context():
+
+        db.create_all()
+        inicializar_materiales()
     
     return app
