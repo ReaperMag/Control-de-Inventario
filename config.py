19944d172a600c725ffae0fba4408e2033f0d3fb
Jair Mera <j.meracount@gmail.com>
2024-12-08 23:57:23 -0500

    Postgres update 2

--- config.py
+++ config.py
@@ -7,4 +7,11 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY","NOSE123")
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 10,
+        'max_overflow': 20,
+        'pool_timeout': 30,
+        'pool_recycle': 1800,
+        'pool_pre_ping': True,
+    }
     
